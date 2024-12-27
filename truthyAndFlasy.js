fbe72d64949b61308afc76a77c9b5bf6cb277f9a
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2024-12-28 03:28:24 +0600

    truthy & falsy

---
+++ truthyAndFlasy.js
@@ -0,0 +1,20 @@
+let num = NaN;
+if (num) {
+    console.log('Truthy');
+} else {
+    console.log('Falsy');
+}
+
+ 
+if (num=-1) {
+    console.log('Truthy');
+} else {
+    console.log('Falsy');
+}
+
+
+if (false) {
+    console.log('Truthy');
+} else {
+    console.log('Falsy');
+}
