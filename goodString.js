e12b46881ad2d194e08977b0aaadb12bab50a7df
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2024-12-27 15:19:56 +0600

    js

--- goodString.js
+++ goodString.js
@@ -1,1 +1,17 @@
-// 
+// A good string is a string that starts with the letter 'a' & has a length of <3. Write a program to find if a string is good or not.
+
+
+let str = 'bapple';
+if (str[0] === 'a' && str.length < 3) {
+    console.log('Good String');
+} else {
+    console.log('not a good String');
+}
+
+
+let num=12;
+
+if((num % 3 === 0) && ((num+1 === 15) || (num-1 === 11))){
+    console.log('true');}else{
+        console.log('false');
+    };
